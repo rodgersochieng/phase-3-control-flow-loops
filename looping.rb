36fc99df7c0ae9eee81a2b62dc254361d0aece71
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-10-21 11:43:49 +0300

    completed lab

--- looping.rb
+++ looping.rb
@@ -1,7 +1,19 @@
+# def happy_new_year
+#   conter = 10
+#   until counter == 0
+#     puts counter
+#     counter -= 1
+#   end
+#   puts "Happy New Year!"
+# end
 def happy_new_year
-  # your code here
+  counter = 10
+  until counter == 0
+    puts counter
+    counter -= 1
+  end
+  puts "Happy New Year!"
 end
-
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
@@ -16,9 +28,22 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+(1..100).each do |num|
+  puts fizzbuzz(num)
+end
 end
 
+# def reverse_string(str)
+#   reverse_string = "hello"
+#   str.length.times do |i|
+#     reverse_str = str[i] + reverse_str
+#   end
+#   puts reverse_str
+# end
 def reverse_string(str)
-  # your code here
+  reversed_str = ""
+  str.length.times do |i|
+    reversed_str = str[i] + reversed_str
+  end
+  reversed_str
 end
